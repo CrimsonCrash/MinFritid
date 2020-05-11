833fd4b85c15e4097422f680ade1a75deb3c7848
TobiasHBrandt <tobias.h.brandt@hotmail.com>
2020-05-11 14:26:10 +0200

    routing og nogle package

--- MinFritidAngular/src/app/app-routing.module.ts
+++ MinFritidAngular/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ForsideComponent } from "./forside/forside.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: ForsideComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponent = [ForsideComponent]
--- MinFritidAngular/src/app/app.module.ts
+++ MinFritidAngular/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForsideComponent } from './forside/forside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ForsideComponent
+    ForsideComponent,
+    routingComponent
   ],
   imports: [
     BrowserModule,
