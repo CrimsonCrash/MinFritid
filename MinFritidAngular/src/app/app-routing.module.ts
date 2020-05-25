9f90d72435e353593bdf8027aece3d109ed8c284
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2020-05-25 13:00:17 +0200

    link
    link til de andre componenter i menuen

--- MinFritidAngular/src/app/app-routing.module.ts
+++ MinFritidAngular/src/app/app-routing.module.ts
@@ -4,12 +4,32 @@ import { ForsideComponent } from "./forside/forside.component";
 import { AktivitetComponent } from './aktivitet/aktivitet.component';
 import { LoginComponent } from './login/login.component';
 import { OpretBrugerComponent } from './opret-bruger/opret-bruger.component'
+import { OmosComponent } from './om-os/om-os.component';
+import { BrugerProfilComponent } from './bruger-profil/bruger-profil.component';
+import { FavoritterComponent } from './favoritter/favoritter.component';
+import { KontaktComponent } from './kontakt/kontakt.component';
+import { MinProfilComponent } from './min-profil/min-profil.component';
+import { MineAktiviteterComponent } from './mine-aktiviteter/mine-aktiviteter.component';
+import { NyAktivitetComponent } from './ny-aktivitet/ny-aktivitet.component';
+import { RedigerAktivitetComponent } from './rediger-aktivitet/rediger-aktivitet.component';
+import { SoegComponent } from './soeg/soeg.component';
+import { VennerComponent } from './venner/venner.component';
 
 const routes: Routes = [
   {path: '', component: ForsideComponent},
   {path: 'aktivitet', component: AktivitetComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'opretBruger', component: OpretBrugerComponent}
+  {path: 'opretBruger', component: OpretBrugerComponent},
+  {path: 'omOs', component: OmosComponent},
+  {path: 'minProfil', component: MinProfilComponent},
+  {path: 'brugerProfil', component: BrugerProfilComponent},
+  {path: 'favorit', component: FavoritterComponent},
+  {path: 'kontakt', component: KontaktComponent},
+  {path: 'mineAktiviteter', component: MineAktiviteterComponent},
+  {path: 'nyAktivitet', component: NyAktivitetComponent},
+  {path: 'redigerAktivitet', component: RedigerAktivitetComponent},
+  {path: 'søg', component: SoegComponent},
+  {path: 'venner', component: VennerComponent}
 ];
 
 @NgModule({
@@ -17,4 +37,6 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponent = [ForsideComponent, AktivitetComponent]
+export const routingComponent = [ForsideComponent, AktivitetComponent, OmosComponent, BrugerProfilComponent,
+  FavoritterComponent, KontaktComponent, MinProfilComponent, MineAktiviteterComponent, NyAktivitetComponent,
+  RedigerAktivitetComponent, SoegComponent, VennerComponent]
