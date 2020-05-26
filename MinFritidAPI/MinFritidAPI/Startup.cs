f03f716e1a493eec7a024cfd7ca71286e4cee876
CrimsonCrash <39299119+CrimsonCrash@users.noreply.github.com>
2020-05-26 14:00:31 +0200

    nogle mindre ændringer

--- MinFritidAPI/MinFritidAPI/Startup.cs
+++ MinFritidAPI/MinFritidAPI/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using MinFritidAPI.Data;
 
 namespace MinFritidAPI
