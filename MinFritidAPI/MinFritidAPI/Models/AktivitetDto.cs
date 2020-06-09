acb0fabfa7215882f457af45cd191052af673ade
Rasmus Rask <35446411+SaintRask@users.noreply.github.com>
2020-06-09 07:43:06 +0200

    AktivitetController + DTO's
    Aktivitet henter også en liste af tilmeldte brugere

---
+++ MinFritidAPI/MinFritidAPI/Models/AktivitetDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace MinFritidAPI.Controllers
+{
+    internal class AktivitetDto
+    {
+        public int AktivitetID { get; set; }
+        public string Titel { get; set; }
+        public string Beskrivelse { get; set; }
+        public IEnumerable<BrugerDto> ABTilmeldt { get; set; }
+    }
+}
