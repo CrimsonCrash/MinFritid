a84f1b6fa1d1a3407120c3f3783008e846dedf39
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2020-06-24 14:56:01 +0200

    login

--- MinFritidAngular/src/app/data.service.ts
+++ MinFritidAngular/src/app/data.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient,HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Ibruger } from './data/Ibruger';
-import { Iaktivitet } from './data/Iaktivitet';
-
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Ibruger } from "./data/Ibruger";
+import { Iaktivitet } from "./data/Iaktivitet";
 
 const httpOptions = {
     headers: new HttpHeaders({
@@ -16,81 +15,113 @@ const httpOptions = {
     providedIn: "root",
 })
 export class DataService {
-  [x: string]: any;
-  header: any;
-  constructor(private http: HttpClient) { 
-    // const headerSettings: {[name: string]: string | string[];} = {};
-    // this.header = new HttpHeaders(headerSettings);
-  }
+    [x: string]: any;
+    header: any;
+    constructor(private http: HttpClient) {
+        // const headerSettings: {[name: string]: string | string[];} = {};
+        // this.header = new HttpHeaders(headerSettings);
+    }
 
-  getBrugers(): Observable<Ibruger[]>{
-    return this.http.get<Ibruger[]>('http://localhost:5001/api/bruger/')
+    getBrugers(): Observable<Ibruger[]> {
+        return this.http.get<Ibruger[]>("http://localhost:5001/api/bruger/");
+    }
 
-  }
+    getBruger(brugerId) {
+        return this.http.get("http://localhost:5001/api/bruger/" + brugerId);
+    }
+    createBruger(bruger: Ibruger): Observable<Ibruger> {
+        //const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/text'}) };
+        return this.http.post<Ibruger>(
+            "http://localhost:5001/api/account/register",
+            bruger,
+            httpOptions
+        );
+    }
+    login(email: string, password: string) {
+        return this.http
+            .post<any>("http://localhost:5001/api/account/login/", {
+                email,
+                password,
+            })
+            .pipe(
+                map((user) => {
+                    if (user && user.token) {
+                        localStorage.setItem("TokenInfo", JSON.stringify(user));
+                    }
+                    return user;
+                })
+            );
+    }
 
-  getBruger(brugerId) {
-    return this.http.get('http://localhost:5001/api/bruger/'+ brugerId)
-  }
-  postBrugers(bruger: Ibruger): Observable<Ibruger>{
-    //const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/text'}) };
-    return this.http.post<Ibruger>('http://localhost:5001/api/account/register', bruger, httpOptions)
-  }
-  login(email: string, password: string) {
-    return this.http.post<any>('http://localhost:5001/api/account/login/', { email, password})
-    .pipe(map(user => {
-      if(user && user.token) {
-        localStorage.setItem('TokenInfo', JSON.stringify(user));
-      }
-      return user;
-    }));
-  }
+    logout() {
+        localStorage.removeItem("TokenInfo");
+    }
 
-  logout() {
-    localStorage.removeItem('TokenInfo');
-  }
+    // postBrugers(bruger: Ibruger): Observable<Ibruger>{
+    //   return this.http.post<Ibruger>('http://localhost:5001/api/bruger/', bruger, httpOptions)
+    // }
 
-  // postBrugers(bruger: Ibruger): Observable<Ibruger>{
-  //   return this.http.post<Ibruger>('http://localhost:5001/api/bruger/', bruger, httpOptions)
-  // }
+    delBruger(brugerId) {
+        return this.http.delete("http://localhost:5001/api/bruger/" + brugerId);
+    }
 
-  delBruger(brugerId) {
-    return this.http.delete('http://localhost:5001/api/bruger/'+ brugerId)
-  }
+    putBrugers(bruger: Ibruger, brugerId): Observable<Ibruger> {
+        return this.http.put<Ibruger>(
+            "http://localhost:5001/api/bruger/" + brugerId,
+            bruger,
+            httpOptions
+        );
+    }
 
-  putBrugers(bruger: Ibruger, brugerId): Observable<Ibruger>{
-    return this.http.put<Ibruger>('http://localhost:5001/api/bruger/' + brugerId, bruger, httpOptions)
-  }
+    aktiverBruger(brugerId) {
+        return this.http.get(
+            "http://localhost:5001/api/bruger/aktiv/" + brugerId
+        );
+    }
 
-  aktiverBruger(brugerId) {
-    return this.http.get('http://localhost:5001/api/bruger/aktiv/'+ brugerId)
-  }
+    deaktiverBruger(brugerId) {
+        return this.http.get(
+            "http://localhost:5001/api/bruger/deaktiv/" + brugerId
+        );
+    }
 
-  deaktiverBruger(brugerId) {
-    return this.http.get('http://localhost:5001/api/bruger/deaktiv/'+ brugerId)
-  }
+    getAktiviteter(): Observable<Iaktivitet[]> {
+        return this.http.get<Iaktivitet[]>(
+            "http://localhost:5001/api/aktivitet/"
+        );
+    }
 
-  getAktiviteter(): Observable<Iaktivitet[]>{
-    return this.http.get<Iaktivitet[]>('http://localhost:5001/api/aktivitet/') 
-  }
+    getAktivitet(aktivitetId) {
+        return this.http.get(
+            "http://localhost:5001/api/aktivitet/" + aktivitetId
+        );
+    }
 
-  getAktivitet(aktivitetId){
-    return this.http.get('http://localhost:5001/api/aktivitet/' + aktivitetId) 
-  }
+    postAktivitet(aktivitet: Iaktivitet): Observable<Iaktivitet> {
+        return this.http.post<Iaktivitet>(
+            "http://localhost:5001/api/aktivitet/",
+            aktivitet,
+            httpOptions
+        );
+    }
 
-  postAktivitet(aktivitet : Iaktivitet): Observable<Iaktivitet>{
-    return this.http.post<Iaktivitet>('http://localhost:5001/api/aktivitet/', aktivitet, httpOptions)
-  }
+    putAktivitet(aktivitet: Iaktivitet, brugerId): Observable<Iaktivitet> {
+        return this.http.put<Iaktivitet>(
+            "http://localhost:5001/api/aktivitet/" + brugerId,
+            aktivitet,
+            httpOptions
+        );
+    }
 
-  putAktivitet(aktivitet : Iaktivitet, brugerId): Observable<Iaktivitet>{
-    return this.http.put<Iaktivitet>('http://localhost:5001/api/aktivitet/' + brugerId, aktivitet, httpOptions)
-  }
+    delAktivitet(aktivitetId) {
+        return this.http.delete(
+            "http://localhost:5001/api/aktivitet/" + aktivitetId
+        );
+    }
 
-  delAktivitet(aktivitetId) {
-    return this.http.delete('http://localhost:5001/api/aktivitet/'+ aktivitetId)
-  }
-
-  deaktivateAktivitet(brugerId){
-    return this.http.get('http://localhost:5001/api/aktivitet/deaktiv/' + brugerId)
-  }
-
+    deaktivateAktivitet(brugerId) {
+        return this.http.get(
+            "http://localhost:5001/api/aktivitet/deaktiv/" + brugerId
+        );
+    }
 }
--- MinFritidAngular/src/app/login/login.component.ts
+++ MinFritidAngular/src/app/login/login.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { first } from "rxjs/operators";
+import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"],
 })
 export class LoginComponent implements OnInit {
+    submitClick = false;
+    submitted = false;
+    loginForm: FormGroup;
+    returnUrl: string;
+    error = "";
 
-  constructor(private route: ActivatedRoute) { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private dataService: DataService
+    ) {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.loginForm = this.formBuilder.group({
+            Email: ["", [Validators.required]],
+            Password: ["", [Validators.required]],
+        });
 
+        this.dataService.logout();
+
+        this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+    }
+
+    onLogin() {}
 }
