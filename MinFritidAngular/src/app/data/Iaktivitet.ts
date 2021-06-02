b574a4ced9a246bc03e9c2026cff5a2dd485880f
TobiasHBrandt <39334625+TobiasHBrandt@users.noreply.github.com>
2021-06-02 14:24:40 +0200

    aktivitet forside
    visse aktivitet på forside

--- MinFritidAngular/src/app/data/Iaktivitet.ts
+++ MinFritidAngular/src/app/data/Iaktivitet.ts
@@ -1,32 +1,32 @@
 import { IaktivitetBrugerTilmeldt } from './IaktivitetBrugerTilmeldt';
 
 export class Iaktivitet {
-    constructor
-    (
-        AktivitetID: number,
-        BrugerID: number,
-        Titel: string,
-        Beskrivelse: string,
-        Huskeliste: string,
-        Pris: number,
-        MaxDeltagere: number,
-        StartTidspunkt: Date,
-        SlutTidspunkt: Date,
-        PostNummer: number,
-        AktivitetBrugerTilmeldte: IaktivitetBrugerTilmeldt[],
-        Aktiv: boolean
-    )
-    {}
-    // _AktivitetID: number;
-    // _BrugerID: number;
-    // _Titel: string;
-    // _Beskrivelse: string;
-    // _Huskeliste: string;
-    // _Pris: number;
-    // _MaxDeltagere: number;
-    // _StartTidspunkt: Date;
-    // _SlutTidspunkt: Date;
-    // _PostNummer: number;
-    // _AktivitetBrugerTilmeldte: IaktivitetBrugerTilmeldt[];
-    // _Aktiv: boolean
+    // constructor
+    // (
+    //     AktivitetID: number,
+    //     BrugerID: number,
+    //     Titel: string,
+    //     Beskrivelse: string,
+    //     Huskeliste: string,
+    //     Pris: number,
+    //     MaxDeltagere: number,
+    //     StartTidspunkt: Date,
+    //     SlutTidspunkt: Date,
+    //     PostNummer: number,
+    //     AktivitetBrugerTilmeldte: IaktivitetBrugerTilmeldt[],
+    //     Aktiv: boolean
+    // )
+    // {}
+    AktivitetID: number;
+    BrugerID: number;
+    Titel: string;
+    Beskrivelse: string;
+    Huskeliste: string;
+    Pris: number;
+    MaxDeltagere: number;
+    StartTidspunkt: Date;
+    SlutTidspunkt: Date;
+    PostNummer: number;
+    AktivitetBrugerTilmeldte: IaktivitetBrugerTilmeldt[];
+    Aktiv: boolean
 }
